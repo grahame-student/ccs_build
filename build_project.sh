4f8eef64502b47662b61cf79c54b9a77eec96461
Grahame White <57143548+grahame-student@users.noreply.github.com>
2022-06-25 19:21:04 +0100

    Update build_project.sh

--- build_project.sh
+++ build_project.sh
@@ -5,7 +5,8 @@ echo "$@"
 
 mkdir workspace
 
-ls -la /opt/ti/ccs1100
+ls -la /opt/ti/ccs1100/ccs
+ls -la /opt/ti/ccs1100/Code\ Composer\ Studio\ 11.0.0.desktop
 
 eclipse -noSplash -data "workspace" -application com.ti.ccstudio.apps.projectImport -ccs.location "$1"
 eclipse -noSplash -data "workspace" -application com.ti.ccstudio.apps.projectBuild -ccs.projects "$2" --ccs.configuration "$3"
