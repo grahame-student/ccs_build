a6ec16b2997b73ffea5d3846da532e5ee9c7e208
Grahame White <57143548+grahame-student@users.noreply.github.com>
2022-06-25 16:45:29 +0100

    Added commands to import / build project

--- build_project.sh
+++ build_project.sh
@@ -4,4 +4,5 @@ echo build_project.sh has been run with the following arguments
 echo "$@"
 
 mkdir workspace
-ls -l
+eclipse -noSplash -data "workspace" -application com.ti.ccstudio.apps.projectImport -ccs.location "$1"
+eclipse -noSplash -data "workspace" -application com.ti.ccstudio.apps.projectBuild -ccs.projects "$2" --ccs.configuration "$3"
