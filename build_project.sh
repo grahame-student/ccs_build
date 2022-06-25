7d295c82b2cff39d00aca186dfa239c5e2e5c5ae
Grahame White <57143548+grahame-student@users.noreply.github.com>
2022-06-25 18:05:04 +0100

    Update build_project.sh

--- build_project.sh
+++ build_project.sh
@@ -4,5 +4,8 @@ echo build_project.sh has been run with the following arguments
 echo "$@"
 
 mkdir workspace
+
+ls -la /opt/ti
+
 eclipse -noSplash -data "workspace" -application com.ti.ccstudio.apps.projectImport -ccs.location "$1"
 eclipse -noSplash -data "workspace" -application com.ti.ccstudio.apps.projectBuild -ccs.projects "$2" --ccs.configuration "$3"
