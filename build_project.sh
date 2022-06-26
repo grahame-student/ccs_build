a851cd46e3cd39428be52131f6d80dd9d1934207
Grahame White <57143548+grahame-student@users.noreply.github.com>
2022-06-26 16:29:40 +0100

    Update build_project.sh

--- build_project.sh
+++ build_project.sh
@@ -17,10 +17,10 @@ ls -la /opt/ti/ccs1100/ccs
 
 echo "import project into workspace"
 echo "/opt/ti/ccs1100/ccs/eclipse -noSplash -data "/workspace" -application com.ti.ccstudio.apps.projectImport -ccs.location "./$1/""
-echo 
+echo ""
 /opt/ti/ccs1100/ccs/eclipse -noSplash -data "/workspace" -application com.ti.ccstudio.apps.projectImport -ccs.location "./$1/" | tr -d '\r'
 
 echo "build project"
 echo "/opt/ti/ccs1100/ccs/eclipse -noSplash -data "/workspace" -application com.ti.ccstudio.apps.projectBuild  -ccs.projects "./$2/" --ccs.configuration "$3""
-echo
+echo ""
 /opt/ti/ccs1100/ccs/eclipse -noSplash -data "/workspace" -application com.ti.ccstudio.apps.projectBuild  -ccs.projects "./$2/" --ccs.configuration "$3" | tr -d '\r'
