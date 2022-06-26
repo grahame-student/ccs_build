4e14788e59dad232b908f7c5b92b62080cb07b00
Grahame White <57143548+grahame-student@users.noreply.github.com>
2022-06-26 19:58:39 +0100

    Update build_project.sh

--- build_project.sh
+++ build_project.sh
@@ -3,17 +3,8 @@
 echo build_project.sh has been run with the following arguments
 echo "$@"
 
-echo permissions
-id
-
-echo working directory
-ls -la
-
-echo installation dir
-ls -la /opt/ti/ccs1100/ccs
-
 echo import project into workspace
-/opt/ti/ccs1100/ccs/eclipse -noSplash -data "/home/build/workspace" -application com.ti.ccstudio.apps.projectImport -ccs.location "./$1"
+/opt/ti/ccs1100/ccs/eclipse/eclipse -noSplash -data "/home/build/workspace" -application com.ti.ccstudio.apps.projectImport -ccs.location "$1"
 
 echo build project
-/opt/ti/ccs1100/ccs/eclipse -noSplash -data "/home/build/workspace" -application com.ti.ccstudio.apps.projectBuild  -ccs.projects "./$2" --ccs.configuration "$3"
+/opt/ti/ccs1100/ccs/eclipse/eclipse -noSplash -data "/home/build/workspace" -application com.ti.ccstudio.apps.projectBuild  -ccs.workspace --ccs.configuration "$3"
