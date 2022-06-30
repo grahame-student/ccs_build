085c77a1c2c1f2b6c2a5b8bb1725a17a444e4cb5
Grahame White <57143548+grahame-student@users.noreply.github.com>
2022-06-30 14:20:10 +0100

    Update build_project.sh

--- build_project.sh
+++ build_project.sh
@@ -1,7 +1,27 @@
 #!/bin/sh -l
 
-echo build_project.sh has been run with the following arguments
-echo "$@"
+####################################
+###   Helper Functions Go Here   ###
+####################################
+
+# Check CCS project files exist at specified location
+Check_CCS_Project_Files() {
+    file1="$1/.project"
+    file2="$1/.cproject"
+    file3="$1/.ccsproject"
+
+    if [ -a $file1 ] && [ -a $file2 ] && [ -a $file3 ]
+    then echo "CCS project files found"
+    else echo "CCS project files not found" && exit 1
+    fi
+}
+
+
+
+####################################
+### Main Action Code Starts Here ###
+####################################
+Check_CCS_Project_Files $1
 
 echo import project into workspace
 /opt/ti/ccs1100/ccs/eclipse/eclipse -noSplash -data "/home/build/workspace" -application com.ti.ccstudio.apps.projectImport -ccs.location "$1"
